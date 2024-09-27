d7f6092f01e232178a56d0042d2126c677fe05ad
rupesh1395 <121029835+rupesh1395@users.noreply.github.com>
2024-09-27 13:52:33 +0530

    Delete HomePage.ts

--- HomePage.ts
+++
@@ -1,9 +0,0 @@
-import { Selector, t } from 'testcafe';
-
-export default class HomePage {
-    static mobileLinkSelector: Selector = Selector('[data-csa-c-content-id="nav_cs_mobiles"]');
-    
-    public static async clickOnMobileLink(): Promise<any> {
-        await t.doubleClick(this.mobileLinkSelector)
-    }
-}
