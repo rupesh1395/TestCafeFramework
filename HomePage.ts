d2f4dc4a067be489187d6214a023a1f16b5cccf9
rupesh1395 <121029835+rupesh1395@users.noreply.github.com>
2024-09-27 09:26:29 +0530

    Delete HomePage.ts

--- HomePage.ts
+++
@@ -1,9 +0,0 @@
-import { Selector, t } from 'testcafe';
-
-export default class HomePage {
-    static ButtonSelector: Selector = Selector('[data-csa-c-content-id="nav_cs_mobiles"]')
-
-    public static async clickOnButton(): Promise<any> {
-        await t.click(this.ButtonSelector)
-    }
-}
